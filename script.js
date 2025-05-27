486186c16804dc5f3518138f5d0917dcb6dfe459
Lamzef Achraf <achraf.lamzef.86@edu.uiz.ac.ma>
2025-05-27 11:02:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 document.getElementById('bmi-form').addEventListener('submit', function(e) {
   e.preventDefault();
+  const height = parseFloat(document.getElementById('height').value) / 100;
+  const weight = parseFloat(document.getElementById('weight').value);
